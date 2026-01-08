7abce2bf731e33009c4cf39628442bc1bbe5c0bd
Vinay Rajpoot <vinayrajpoot@Mac.lan>
2026-01-08 13:24:56 +0530

    payment service implemented successfully

---
+++ src/main/java/com/homemate/email_service/controller/EmailController.java
@@ -0,0 +1,28 @@
+package com.homemate.email_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestBody String message) {
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo("vinayrajpoot64@gmail.com");   // demo receiver
+        mail.setSubject("Service Booking Confirmation");
+        mail.setText(message);
+
+        mailSender.send(mail);
+        return "EMAIL_SENT";
+    }
+}
+
